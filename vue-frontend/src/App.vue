03852222e67dddd8547d3bf44f58d59312bbb797
Lucas Nunes <lucas.nunes.chizukovr@gmail.com>
2020-03-29 00:16:39 -0300

    frontend em vuejs

---
+++ vue-frontend/src/App.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap');
+
+* {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font: 400 14px Roboto, sans-serif;
+    background: #f0f0f5;
+    -webkit-font-smoothing: antialiased;
+}
+
+input, button, textarea {
+    font: 400 18px Roboto, sans-serif;
+}
+
+button {
+    cursor: pointer;
+}
+
+form input {
+    width: 100%;
+    height: 60px;
+    color: #333;
+    border: 1px solid #dcdce6;
+    border-radius: 8px;
+    padding: 0 24px;
+}
+
+form textarea {
+    width: 100%;
+    min-height: 140px;
+    color: #333;
+    border: 1px solid #dcdce6;
+    border-radius: 8px;
+    padding: 16px 24px;
+    line-height: 24px;
+    resize: vertical;
+}
+
+.button {
+    width: 100%;
+    height: 60px;
+    background: #e02041;
+    border: 0;
+    border-radius: 8px;
+    color: #fff;
+    font-weight: 700;
+    margin-top: 16px;
+    display: inline-block;
+    text-align: center;
+    text-decoration: none;
+    font-size: 18px;
+    line-height: 60px;
+    transition: filter 0.2s;
+}
+
+.button:hover {
+    filter: brightness(90%);
+}
+
+.svg-link {
+    align-items: center;
+    display: flex;
+    margin-top: 40px;
+    color: #41414d;
+    font-size: 18px;
+    text-decoration: none;
+    font-weight: 500;
+    transition: opacity 0.2s;
+}
+
+.svg-link:hover {
+    opacity: 0.8;
+}
+
+.svg-link svg {
+    margin-right: 8px;
+}
+
+.align-center {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
---
+++ vue-frontend/src/pages/Logon/index.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class='logon-container'>
+      <section class='form'>
+        <img src="{{ logoImg }}" alt='Be The Hero' />
+        <form @submit="handleLogin">
+          <h1> Faça seu logon </h1>
+          <input
+            type="email"
+            v-model="email"
+            required
+            placeholder='Seu e-mail'
+          />
+          <input
+            style="margin-top: 10;"
+            type="password"
+            v-model="password"
+            required
+            placeholder='Sua senha'
+          />
+          <button class='button' type='submit'>
+            Entrar
+          </button>
+      {(deleteAlert ?
+          <div class="confirmDelete">
+              Sua conta foi apagada, é uma pena que decidiu sair, crie uma conta nova quando quiser voltar, estaremos lhe esperando.
+          </div>
+      :
+          ''
+      )}
+      {(expired === true ?
+          <div class="confirmDelete">
+              Sua sessão expirou, faça sua autenticação novamente para continuar.
+          </div>
+      :
+          ''
+      )}
+          <router-link to='/register' class='svg-link'>
+            <FiLogIn size={16} color='#E02041' />
+            Não tenho cadastro
+          </router-link>
+        </form>
+      </section>
+
+      <img src="{{ heroesImg }}" alt='Heroes' />
+    </div>
+</template>
+
+<script>
+import logoImg from "../../assets/logo.svg"
+import heroesImg from "../../assets/heroes.png"
+
+export default {
+    name: 'Logon'
+}
+</script>
+
+<style scoped>
+.logon-container {
+    width: 100%;
+    max-width: 1120px;
+    height: 100vh;
+    margin: 0 auto;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    /* transform: scale(.8); */
+}
+
+.logon-container section.form {
+    width: 100%;
+    max-width: 350px;
+    margin-right: 30px;
+}
+
+.logon-container section.form form {
+    margin-top: 100px;
+}
+
+.logon-container section.form form h1 {
+    font-size: 32px;
+    margin-bottom: 32px;
+}
+
+
+.logon-container section .confirmDelete {
+    border: 1px solid rgb(219, 11, 11);
+    border-radius: 8px;
+    text-align: center;
+    background: rgb(255, 186, 186);
+    padding: 30px 10px;
+    margin-top: 30px;
+    color: rgb(219, 11, 11);
+}
+</style>
