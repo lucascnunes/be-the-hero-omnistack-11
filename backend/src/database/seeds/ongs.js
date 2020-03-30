e161914cf226f57db2b837f7205eb499281698a1
Lucas Nunes <lucas.nunes.chizukovr@gmail.com>
2020-03-29 21:30:40 -0300

    adicionando novos campos de password e timestamps

--- backend/src/database/seeds/ongs.js
+++ backend/src/database/seeds/ongs.js
@@ -7,12 +7,14 @@ exports.seed = function(knex) {
       return knex('ongs').insert([
         {
           id: 1,
-          key: '123abc',
           name: "APAD",
-          email: "apad2@apad.com.br",
+          email: "apad@apad.com.br",
           whatsapp: "3123123123",
           city: "Rio do Sul",
-          uf: "SC"
+          uf: "SC",
+          password: "$2b$10$ws32UHsGRqiNvwW69dabU.t79kCtUvN5JvRbtMV6et9QHwc0UUbda", // 123456789
+          created_at: new Date(),
+          updated_at: new Date()
         },
       ]);
     });
